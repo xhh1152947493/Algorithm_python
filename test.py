e36d8d7435dfccdd4f70a1ff6de97f60b337856a
zzh <1152947493@qq.com>
2019-11-07 17:44:34 +0800

    first commit

---
+++ test.py
@@ -0,0 +1,18 @@
+import random as rd
+
+
+class Test:
+    def __init__(self, row=10, col=10, min_=0, max_=100):
+        self.row = row
+        self.col = col
+        self.min_ = min_
+        self.max_ = max_
+
+    def test_date(self):
+        res = []
+        for _ in range(self.row):
+            tmp = []
+            for _ in range(self.col):
+                tmp.append(rd.randint(self.min_, self.max_))
+            res.append(tmp)
+        return res
